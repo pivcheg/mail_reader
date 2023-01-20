c1383d944d2dfeb92856d2ab3f5e4b577125e202
pivcheg <pivcheg000@gmail.com>
2023-01-20 16:41:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ mail_pass = "пароль от ящика для внешних приложен
 username = "адрес@mail.ru"
 imap_server = "imap.mail.ru"
 
-bot_key = "токен от бота"
+bot_token = "токен от бота"
 chat_id = "айди чата куда добавлен бот"
 
 send_attach = True #пересылка вложений, чтобы отключить нужен параметр False
 
-encoding="utf-8"
+encoding = "utf-8"
